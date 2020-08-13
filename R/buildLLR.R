2a15f2f4952c649220414dcbc3c7d666fa4caf5f
Jochen Weile <jochenweile@gmail.com>
2020-08-13 16:06:51 -0400

    Added license

--- R/buildLLR.R
+++ R/buildLLR.R
@@ -1,5 +1,21 @@
+# Copyright (C) 2020  Jochen Weile, Roth Lab
+#
+# This file is part of maveLLR
+#
+# maveLLR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# maveLLR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with maveLLR  If not, see <https://www.gnu.org/licenses/>.
 
-#' Build LLR function
+#' Build LLR function using kernel density estimation
 #'
 #' @param posScores fitness scores in the positive reference set
 #' @param negScores fitness scores in the negative reference set
@@ -29,7 +45,7 @@ buildLLR.kernel <- function(posScores, negScores, bw=0.1, kernel="gaussian") {
 }
 
 
-#' Build LLR function
+#' Build LLR function using Gaussian densities.
 #'
 #' @param posScores fitness scores in the positive reference set
 #' @param negScores fitness scores in the negative reference set
