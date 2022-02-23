e088e7b2857695612c337b137e8d119f7f2677a5
Jochen Weile <jochenweile@gmail.com>
2022-02-23 10:42:47 -0500

    tunable outlier suppression; Tavtigian's thresholds in plot

--- R/buildLLR.R
+++ R/buildLLR.R
@@ -32,20 +32,28 @@
 #' llr <- buildLLR.kernel(posScores,negScores,bw=0.1,kernel="gaussian")
 #' drawDensityLLR(c(posScores,negScores),llr$llr,llr$posDens,llr$negDens,posScores,negScores)
 #'
-buildLLR.kernel <- function(posScores, negScores, bw=0.1, kernel="gaussian", minCount=1) {
+buildLLR.kernel <- function(posScores, negScores, bw=0.1, kernel="gaussian", outlierSuppression=0.0001) {
 
   library(kdensity)
 
   posDens <- kdensity(posScores,bw=bw,kernel=kernel)
   negDens <- kdensity(negScores,bw=bw,kernel=kernel)
 
+  #generate a dummy outlier point far away from the rest of the distribution
+  # so we can use it to measure it's hypothetical density
   pseudo <- min(posScores) - 10*ifelse(is.numeric(bw),bw,0.1)
+  #measure the hyothetical density of an outlier point
   minDensPos <- kdensity(c(posScores[-1],pseudo),bw=bw,kernel=kernel)(pseudo)
+  #do the same for the negative distribution
   pseudo <- min(negScores) - 10*ifelse(is.numeric(bw),bw,0.1)
   minDensNeg <- kdensity(c(negScores[-1],pseudo),bw=bw,kernel=kernel)(pseudo)
-  minDens <- max(minDensPos,minDensNeg)
+  #whichever outlier density is higher will serve as our uniform prior
+  minDens <- outlierSuppression * max(minDensPos,minDensNeg)
 
-  llrFun <- function(score) sapply(score,function(s)log10(max(posDens(s),minDens)/max(negDens(s),minDens)))
+  llrFun <- function(score) sapply(score,function(s)
+    # log10( max(posDens(s),minDens) / max(negDens(s),minDens) )
+    log10(max(posDens(s),minDens)) - log10(max(negDens(s),minDens))
+  )
 
   return(list(llr=llrFun,posDens=posDens,negDens=negDens))
 }
@@ -130,7 +138,9 @@ buildLLR.gauss <- function(posScores, negScores, spline=TRUE) {
 #' llr <- buildLLR.gauss(posScores,negScores)
 #' drawDensityLLR(c(posScores,negScores),llr$llr,llr$posDens,llr$negDens,posScores,negScores)
 #'
-drawDensityLLR <- function(scores, llrFun, posDens, negDens, posScores, negScores) {
+drawDensityLLR <- function(scores, llrFun, posDens, negDens, posScores, negScores, prior=0.1) {
+
+  llrTs <- llrThresholds(optiLLR(prior))
 
   opar <- par(mfrow=c(2,1))
   xlim <- range(scores,na.rm=TRUE,finite=TRUE)
@@ -139,11 +149,38 @@ drawDensityLLR <- function(scores, llrFun, posDens, negDens, posScores, negScore
     negDens(seq(xlim[[1]],xlim[[2]],length.out = 100))
   ))
   par(mar=c(.1,4,1,1))
-  plot(llrFun,from=xlim[[1]],to=xlim[[2]],xlim=xlim,axes=FALSE,xlab="",ylab="LLR")
+  xs <- seq(xlim[[1]],xlim[[2]],length.out=200)
+  ys <- llrFun(xs)
+  ylim <- range(ys,na.rm=TRUE,finite=TRUE)
+  plot(NA,type="n",xlim=xlim,ylim=ylim,
+    axes=FALSE,xlab="",ylab="LLR"
+  )
+  drawThresh <- function(t,col,label) {
+    if (t >= 0) {
+      if (t < ylim[[2]]) {
+        rect(xlim[[1]],t,xlim[[2]],ylim[[2]],col=col,border=NA)
+        text(.1*xlim[[2]]+.9*xlim[[1]],t,label,pos=3,cex=0.8)
+      }
+    } else {
+      if (t > ylim[[1]]) {
+        rect(xlim[[1]],ylim[[1]],xlim[[2]],t,col=col,border=NA)
+        text(.1*xlim[[2]]+.9*xlim[[1]],t,label,pos=1,cex=0.8)
+      }
+    }
+  }
+  drawThresh(llrTs[["patho.support"]],"lemonchiffon","Patho. support.")
+  drawThresh(llrTs[["patho.moderate"]],"lightgoldenrod1","Patho. moderate")
+  drawThresh(llrTs[["patho.strong"]],"goldenrod1","Patho. strong")
+  drawThresh(llrTs[["patho.vstrong"]],"indianred1","Patho. very str.")
+  drawThresh(llrTs[["benign.support"]],"lemonchiffon","Benign support.")
+  drawThresh(llrTs[["benign.strong"]],"goldenrod1","Benign strong")
+  
+  lines(xs,ys)
+  # plot(llrFun,from=xlim[[1]],to=xlim[[2]],xlim=xlim,axes=FALSE,xlab="",ylab="LLR")
   abline(h=0,col="gray",lty="dashed")
   axis(2)
   par(mar=c(5,4,.1,1))
-  hist(scores,col="gray90",border=NA,freq=FALSE,main="",xlim=xlim,ylim=c(0,ymax))
+  hist(scores,col="gray90",border=NA,freq=FALSE,main="",xlim=xlim,ylim=c(0,ymax),breaks=50)
   plot.function(posDens,from=xlim[[1]],to=xlim[[2]],add=TRUE,col="firebrick3",lwd=2)
   plot.function(negDens,from=xlim[[1]],to=xlim[[2]],add=TRUE,col="darkolivegreen3",lwd=2)
   abline(v=posScores,col="firebrick3")
@@ -152,6 +189,22 @@ drawDensityLLR <- function(scores, llrFun, posDens, negDens, posScores, negScore
 
   return(invisible(NULL))
 }
+
+optiLLR <- function(prior=0.1,posterior=0.9) {
+  log10(posterior*(1-prior)/(prior*(1-posterior)))*4/3
+}
+
+llrThresholds <- function(LLRpvst=optiLLR(0.1),X=2) {
+  c(
+    patho.vstrong=LLRpvst,
+    patho.strong=LLRpvst/X,
+    patho.moderate=LLRpvst/(X^2),
+    patho.support=LLRpvst/(X^3),
+    benign.support=-LLRpvst/(X^3),
+    benign.strong=-LLRpvst/(X^1)
+  )
+}
+
 
 #
 # mthfr <- read.csv("~/projects/mthfr/folate_response_model5.csv")
